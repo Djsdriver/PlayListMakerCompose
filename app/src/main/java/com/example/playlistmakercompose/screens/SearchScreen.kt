d3b7e8700bf3c64a8d42c5b7c972d4b403d1e793
Djsdriver <alexei.gra4ev@gmail.com>
2023-04-26 09:15:57 +0300

    Sprint_9

--- app/src/main/java/com/example/playlistmakercompose/screens/SearchScreen.kt
+++ app/src/main/java/com/example/playlistmakercompose/screens/SearchScreen.kt
@@ -1,11 +1,17 @@
 package com.example.playlistmakercompose.screens
 
+import android.widget.EditText
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.playlistmakercompose.R
 import com.example.playlistmakercompose.routes.Routes
 
@@ -31,7 +37,26 @@ fun SearchScreen(navController: NavController){
                 elevation = 10.dp
             )
         }, content = {
+            var text by remember { mutableStateOf(TextFieldValue()) }
+            return@Scaffold OutlinedTextField(
+                value = text,
+                leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") },
+                //trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) },
+                onValueChange = {
+                    text = it
+                },
+                label = { Text(text = "Email address") },
+                placeholder = { Text(text = "Enter your e-mail") },
+            )
+
 
         })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PrevSearchScreen() {
+    SearchScreen(rememberNavController())
+
 }
 
--- app/src/main/java/com/example/playlistmakercompose/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/playlistmakercompose/screens/SettingsScreen.kt
@@ -1,34 +1,31 @@
 package com.example.playlistmakercompose.screens
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.playlistmakercompose.R
 import com.example.playlistmakercompose.routes.Routes
+import kotlin.reflect.typeOf
 
 
 @Composable
-//@Preview(showBackground = true)
-fun SettingScreen(navController: NavController){
+fun SettingScreen(navController: NavController) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -36,9 +33,11 @@ fun SettingScreen(navController: NavController){
                     Text(text = "Настройки", color = Color.Black)
                 },
                 navigationIcon = {
-                    IconButton(onClick = {navController.navigate(route = Routes.MainScreen.route)}) {
-                        Icon(painter = painterResource(id = R.drawable.ic_arrow_back),
-                            "backIcon", tint = Color.Black)
+                    IconButton(onClick = { navController.navigate(route = Routes.MainScreen.route) }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_arrow_back),
+                            "backIcon", tint = Color.Black
+                        )
                     }
                 },
                 backgroundColor = Color.White,
@@ -51,57 +50,103 @@ fun SettingScreen(navController: NavController){
                     modifier = Modifier
                         .fillMaxWidth()
                         .background(Color.White)
-                        .padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically
+                        .padding(15.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(text = "Темная тема")
                     val checkedState = remember { mutableStateOf(false) }
-                    Switch(checked = checkedState.value,
-                        onCheckedChange = {checkedState.value=it},)
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.White)
-                        .padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "Поделиться приложением")
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(painter = painterResource(id = R.drawable.ic_baseline_share),
-                            contentDescription = "Share", tint = Color(0xFFAEAFB4)
-                        )
-                    }
+                    Switch(
+                        checked = checkedState.value,
+                        onCheckedChange = { checkedState.value = it },
+                    )
                 }
 
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.White)
-                        .padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "Написать в поддержку")
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(painter = painterResource(id = R.drawable.ic_baseline_support),
-                            contentDescription = "Share", tint = Color(0xFFAEAFB4)
-                        )
-                    }
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.White)
-                        .padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "Пользовательское соглашение")
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(painter = painterResource(id = R.drawable.ic_baseline_navigate_next),
-                            contentDescription = "Share", tint = Color(0xFFAEAFB4)
-                        )
-                    }
-                }
+                ItemsSettingScreen(
+                    text = "Поделиться приложением",
+                    painter = painterResource(id = R.drawable.ic_baseline_share),
+                    textDescription = "Share",
+                    intent = Intent(Intent.ACTION_SEND)
+                        .setType("text/plain")
+                        .putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/")
+                )
+
+                ItemsSettingScreen(
+                    text = "Написать в поддержку",
+                    painter = painterResource(id = R.drawable.ic_baseline_support),
+                    textDescription = "Support",
+                    intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
+                        .putExtra(Intent.EXTRA_EMAIL, arrayOf("alexei.gra4ev@gmail.com"))
+                        .putExtra(Intent.EXTRA_SUBJECT, "getString(R.string.TEXT_EXTRA_SUBJEC)")
+                        .putExtra(Intent.EXTRA_TEXT, "getString(R.string.BODY_EXTRA_TEXT)")
+                )
+
+
+                ItemsSettingScreen(
+                    text = "Пользовательское соглашение",
+                    painter = painterResource(id = R.drawable.ic_baseline_navigate_next),
+                    textDescription = "Next",
+                    intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+                )
 
             }
 
-
-
         })
+}
+
+
+@Composable
+private fun ItemsSettingScreen(
+    text: String, painter: Painter, textDescription: String, intent: Intent
+) {
+    val context = LocalContext.current
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(15.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = text)
+        IconButton(onClick = { context.startActivity(intent) }) {
+            Icon(
+                painter = painter,
+                contentDescription = textDescription, tint = Color(0xFFAEAFB4)
+            )
+        }
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PrevSettingScreens() {
+    SettingScreen(rememberNavController())
+
+}
+
+/*
+//функция с обработкой перехода экранов
+@Composable
+private fun ItemsSettingScreen(
+    text: String, painter: Painter, textDescription: String,
+    navController: NavController, screen: String
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(15.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = text)
+        IconButton(onClick = { navController.navigate(route = screen) }) {
+            Icon(
+                painter = painter,
+                contentDescription = textDescription, tint = Color(0xFFAEAFB4)
+            )
+        }
+    }
+}*/
